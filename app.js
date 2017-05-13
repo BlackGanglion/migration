f1283c6fce8d85f4f8e4043de4d97c03a7f129b1
BlackGanglion <BlackGanglion@blackganglion.com>
2017-05-13 14:14:39 +0800

    feat: init project

---
+++ app.js
@@ -0,0 +1,21 @@
+const file = require('./file');
+
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/view/index.html');
+});
+
+io.on('connection', function (client) {
+  client.on('filename', function (filename) {
+    file.tailf(filename, err => {
+      console.log('err: ' + err);
+    }, data => {
+      client.emit(filename, data);
+    });
+  });
+});
+
+server.listen(3000);
---
+++ file.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const fs = require('fs');
+
+/**
+ * tailf
+ *
+ * @param {String} filename 文件名
+ * @param {Function} onError 操作出错时的回调函数，onError(err)
+ * @param {Function} onData 读取到文件内容时的回调函数，onData(data)
+ */
+exports.tailf = function (filename, onError, onData) {
+  // 每次读取文件块大小，16K
+  const CHUNK_SIZE = 16 * 1024;
+  // 打开文件，获取文件句柄
+  fs.open(filename, 'r', (err, fd) => {
+    if (err) return onError(err);
+
+    // 文件开始位置
+    fs.fstat(fd, (err, stats) => {
+      if (err) return onError(err);
+
+      // 文件开始位置
+      let position = 0;
+      // 循环读取
+      const loop = () => {
+        const buf = new Buffer(CHUNK_SIZE);
+        fs.read(fd, buf, 0, CHUNK_SIZE, position,
+          (err, bytesRead, buf) => {
+            if (err) return onError(err);
+
+            // 实际读取的内容长度以 bytesRead 为准，并且更新 position 位置
+            position += bytesRead;
+            onData(buf.slice(0, bytesRead).toString());
+
+            if (bytesRead < CHUNK_SIZE) {
+              // 如果当前已到达文件末尾，则等待 change 事件再尝试读取
+            } else {
+              loop();
+            }
+          });
+      };
+      loop();
+
+      // 监听文件变化，如果收到 change 事件则尝试读取文件内容
+      fs.watch(filename, (event, filename) => {
+        if (event === 'change') {
+          loop();
+        }
+      });
+    });
+  });
+}
